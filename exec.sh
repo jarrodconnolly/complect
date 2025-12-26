4542090c342786d095d0e2a10e774260da8888d8
Jarrod Connolly <jarrod@nestedquotes.ca>
2025-12-26 15:17:21 -0800

    add explicit free command add auto free on string assignment add free temp variable in expressions

--- exec.sh
+++ exec.sh
@@ -7,5 +7,5 @@ complect -b llvm -f fixtures/${1} -o output/${1}.ll
 llc output/${1}.ll -o output/${1}.s
 clang output/${1}.ll -s -o output/${1}
 ./output/${1}
-
+valgrind --tool=memcheck --leak-check=full ./output/${1} 2>&1 | grep -E "(allocs| frees| allocated| lost)"
 
